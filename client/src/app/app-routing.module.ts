5b8d150aa02fd6908b37705119739744497a4e85
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-28 22:43:14 +0100

    chore: adds shared module and graphics module to app

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'graphics-view',
+    loadChildren: async () => {
+      const { GraphicsModule } = await import('./graphics/graphics.module');
+      return GraphicsModule;
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
